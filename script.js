4d03a9358b219c67d1f855e02ee40d04734865c2
lipongyang <67255646+lipongyang@users.noreply.github.com>
2021-12-31 16:43:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function compute()
                 t = document.getElementById ("year").value;
                 r = document.getElementById ("rate").value;
                 si = parseInt((p*t*r)/100 );
-                document.getElementById ('num').innerHTML ="If you deposite : "+"<span class='highlight'>"+p+"</span>";
+                document.getElementById ('num').innerHTML ="If you deposit : "+"<span class='highlight'>"+p+"</span>";
                 
                 document.getElementById ('num1').innerHTML ="at the rate of: "+"<span class='highlight'>"+r+"</span>";
                 document.getElementById ('num2').innerHTML ="you will receive an amount of: "+"<span class='highlight'>"+si+"</span>";
